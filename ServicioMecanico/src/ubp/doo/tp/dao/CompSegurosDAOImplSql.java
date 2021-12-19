89f25b6cc8a9e39a43ec1b0f646eab47ac98c7ee
fbettic <federicobettic@gmail.com>
2021-12-19 01:15:52 -0300

    Calendar

--- ServicioMecanico/src/ubp/doo/tp/dao/CompSegurosDAOImplSql.java
+++ ServicioMecanico/src/ubp/doo/tp/dao/CompSegurosDAOImplSql.java
@@ -22,7 +22,7 @@ public class CompSegurosDAOImplSql implements CompSegurosDAO{
 
     private ConexionSql conexion = null;
     
-    public void CompSegurosDAO(){
+    public CompSegurosDAOImplSql(){
         conexion = ConexionSql.getInstancia();
     }
     
@@ -50,8 +50,8 @@ public class CompSegurosDAOImplSql implements CompSegurosDAO{
         }
         finally{
             try {
-                rs.close();
-                sentencia.close();
+                    sentencia.close();
+                    rs.close();                         
             }
             catch (SQLException ex){
                 System.err.println(ex);
@@ -86,7 +86,7 @@ public class CompSegurosDAOImplSql implements CompSegurosDAO{
         }finally{
             try{
                 sentencia.close();
-                rs.close();
+                rs.close(); 
             }catch(SQLException ex){
                 System.err.println(ex);
             }
@@ -119,7 +119,7 @@ public class CompSegurosDAOImplSql implements CompSegurosDAO{
         }finally{
             try{
                 sentencia.close();
-                rs.close();
+                rs.close(); 
             }catch(SQLException ex){
                 System.err.println(ex);
             }
